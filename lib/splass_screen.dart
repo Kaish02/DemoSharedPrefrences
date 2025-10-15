11e3690ca86c5aea7a5ff001aff1a53c6b630196
raaz1edugaon <rajmohanedugaon1@gmail.com>
2025-10-15 14:53:27 +0530

    designing splash screen

---
+++ lib/splass_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplassScreen(),
+    );
+  }
+}
+class SplassScreen extends StatefulWidget {
+  const SplassScreen({super.key});
+
+  @override
+  State<SplassScreen> createState() => _SplassScreenState();
+}
+
+class _SplassScreenState extends State<SplassScreen> {
+  @override
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 5),() {
+
+    },);
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/aurth_Splash.png',height: 180,color: Colors.blue,),
+            SizedBox(height: 30,),
+            Text("Demo Share Preferences",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),)
+          ],
+        ),
+      ),
+
+    )
+    );
+  }
+}
+
